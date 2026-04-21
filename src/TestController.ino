ad3d561ece870d99bfeb7ad358ebefc55c414dbb
Fizi3d <fizi3d3@gmail.com>
2026-04-20 22:19:19 -0400

    Create TestController.ino

---
+++ src/TestController.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include <RF24.h>
+
+RF24 radio(3, 5); // CE, CSN
+
+const byte address[6] = "00001";
+
+void setup() {
+  Serial.begin(9600);
+
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setChannel(76);
+
+  radio.openWritingPipe(address);
+  radio.stopListening();
+
+  Serial.println("TX Ready");
+}
+
+void loop() {
+  int data = 1;
+
+  bool ok = radio.write(&data, sizeof(data));
+
+  Serial.print("Sent OK: ");
+  Serial.println(ok);
+
+  delay(500);
+}
